543235d208e9f1d901af2091d799c2354a998c83
samraabashir <samraa6b@gmail.com>
2026-03-19 22:06:33 +0100

    updated html files

--- app.py
+++ app.py
@@ -1,13 +1,34 @@
 
-from flask import Flask
+from flask import Flask, render_template, request
+from models import Question, QuizResult
 
 app = Flask(__name__)
 
-@app.route('/')
+questions = [
+    Question("Capital of Nigeria?", ["Abuja", "Lagos", "Kano"], "Abuja"),
+    Question("2 + 2 = ?", ["3", "4", "5"], "4")
+]
+
+@app.route("/")
 def home():
-    return "Hello, Flask is working!"
+    return render_template("index.html")
 
-if __name__ == '__main__':
+
+@app.route("/quiz", methods=["GET", "POST"])
+def quiz():
+    if request.method == "POST":
+        score = 0
+
+        for i, q in enumerate(questions):
+            answer = request.form.get(f"q{i}")
+            if q.check_answer(answer):
+                score += 1
+
+        result = QuizResult(score)
+        return render_template("result.html", result=result.get_result())
+
+    return render_template("quiz.html", questions=questions)
+
+
+if __name__ == "__main__":
     app.run(debug=True)
-
-    # comment
